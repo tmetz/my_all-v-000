75fb63bd273e7c0d1a8990f3b0ebd90adcc9a7fb
Tammy Metz <tammy@tammymetz.com>
2018-07-22 20:44:18 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,15 @@
 require 'pry'
 
+block_return_values = []
 def my_all?(collection)
-
+  i = 0
+  while i < collection.length
+    block_return_values << yield(collection[i])
+    i = i + 1
+  end
+  if block_return_values.include?(false)
+    false
+  else
+    true
+  end
 end
